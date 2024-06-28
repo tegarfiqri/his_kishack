505b991d894d710c2b34dac0f3e87737880357f7
Tegar <tegarfiqri.b@gmail.com>
2024-06-28 18:05:11 +0700

    first commit

---
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue';
+import Header from './Partials/Header.vue';
+import Sidebar from './Partials/Sidebar.vue';
+
+</script>
+
+<script>
+export default {
+    name: 'AuthLayout',
+    components: {
+        Sidebar,
+        Header
+    },
+    data() {
+        return {
+            isSidebarOpen: false
+        }
+    },
+    methods: {
+        toggleSidebar() {
+            this.isSidebarOpen = !this.isSidebarOpen
+        }
+    }
+}
+</script>
+<template>
+    <div class="flex h-screen">
+        <!-- Sidebar -->
+        <Sidebar :isOpen="isSidebarOpen" />
+
+        <!-- Header and Main Content -->
+        <div class="flex-1 flex-shrink flex-col">
+            <!-- Header -->
+            <Header @toggle-sidebar="toggleSidebar" />
+
+            <!-- Main Content -->
+            <main class="flex-1 p-4">
+                <h2 class="text-2xl font-bold">Main Content</h2>
+                <!-- Add your main content here -->
+            </main>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Layouts/Partials/Sidebar.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+</script>
+
+<template>
+    <div :class="[
+        'fixed inset-0 z-30 bg-gray-800 text-white transition-transform transform md:relative md:translate-x-0',
+        isOpen ? 'translate-x-0 relative' : '-translate-x-full fixed',
+        'w-64 flex-none'
+    ]">
+        <div class="p-4 text-2xl font-bold">CMS Menu</div>
+        <nav class="flex-1">
+            <ul>
+                <!-- <li class="p-4 hover:bg-gray-700">
+                    <router-link to="/dashboard" class="block">Dashboard</router-link>
+                </li> -->
+            </ul>
+        </nav>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Sidebar',
+    props: {
+        isOpen: {
+            type: Boolean,
+            required: true
+        }
+    }
+}
+</script>
